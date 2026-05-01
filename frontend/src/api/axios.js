a776429d946af15b48ca4d11414caf3dcd3e33e8
Adi1490tya <adisingh1490@gmail.com>
2026-05-01 15:21:20 +0530

    api fix

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,9 +2,6 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://team-task-manager-production-7cbf.up.railway.app/api",
-  headers: {
-    "Content-Type": "application/json",
-  },
 });
 
 export default api;
